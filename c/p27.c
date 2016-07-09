a06211e50db935d9ef9ea2f8919afd7ef060ab00
scott <scott@cogolabs.com>
2016-07-09 14:06:11 -0400

    break out func

--- c/p27.c
+++ c/p27.c
@@ -5,33 +5,34 @@ int quadratic(long n, long a, long b) {
   return n*n + (a * n) + b;
 }
 
-int main() {
+int countPrimes(long a, long b) {
+  int n = 0;
   long prod;
+  while(1) {
+    prod = quadratic(n, a, b);
+    if(isPrime(prod)) {
+      n++;
+    } else {
+      return n;
+    }
+  }
+}
+
+
+int main() {
   long streak = 0;
   long max, maxa, maxb;
-  int count;
   for(int i=1000; i>-1000; i--) {
     for(int j=1000; j>-1000; j--) {
-      count++;
-      int n = 0;
-      while(1) {
-        prod = quadratic(n, i, j);
-        if(isPrime(prod)) {
-          streak++;
-          n++;
-        } else {
-          if(max < streak) {
-            max = streak;
-            maxa = i;
-            maxb = j;
-          }
-          streak = 0;
-          break;
-        }
+      streak = countPrimes(i, j);
+      if(max < streak) {
+        max = streak;
+        maxa = i;
+        maxb = j;
       }
     }
   }
-  printf("%ld %ld %ld %d\n", maxa, maxb, max, count);
+  printf("%ld %ld %ld\n", maxa, maxb, max);
   printf("Product of coeffs: %ld \n", maxa * maxb);
 
 
